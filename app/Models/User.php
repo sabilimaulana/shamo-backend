a7cf5ca4ec1c7efe7d7fbc16f8fd3b797c70ea01
sabilimaulana <spacysbl.learn@gmail.com>
2021-12-22 20:40:45 +0700

    Model Aplikasi : Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
+        'phone_number',
+        'roles',
         'password',
+
     ];
 
     /**
@@ -58,4 +62,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'users_id', 'id');
+    }
 }
