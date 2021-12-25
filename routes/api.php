f1fabe1feed65f169261236e25a9c8d3df8fdbc9
sabilimaulana <spacysbl.learn@gmail.com>
2021-12-25 12:49:23 +0700

    API : API Transaksi

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\ProductCategoryController;
 use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\TransactionController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [UserController::class, 'fetch']);
     Route::post('user', [UserController::class, 'updateProfile']);
     Route::post('logout', [UserController::class, 'logout']);
+
+    Route::get('transactions', [TransactionController::class, 'all']);
 });
 
 Route::get('products', [ProductController::class, 'all']);
